895d16beb4c77b926fd90c0cb5e73d70efb5cd17
Tom Stefanou <tomstefanou@yahoo.com>
2017-09-14 09:52:14 +0100

    Removed unused reference.

--- EAGamesLauncher/FormMain.cs
+++ EAGamesLauncher/FormMain.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
 using System.Drawing;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
